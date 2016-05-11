1f03bc5ff7875f4039e79721e2dc7769b21b7028
Erick <erickvaz0617@gmail.com>
2016-05-11 00:52:34 -0700

    Changed migration file

--- db/migrate/20160511015937_create_meetings.rb
+++ db/migrate/20160511015937_create_meetings.rb
@@ -3,9 +3,9 @@ class CreateMeetings < ActiveRecord::Migration
     create_table :meetings do |t|
       t.string :name
       t.datetime :start_time
-      t.references :user, index: true, foreign_key: true
+
       t.timestamps null: false
     end
-    add_index :microposts, [:user_id, :created_at]
+
   end
 end
